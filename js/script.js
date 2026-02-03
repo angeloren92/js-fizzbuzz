0c78d4a6b0b43007e82beb5467f9943dc11b227f
angeloren92 <angeloren92@gmail.com>
2026-02-03 14:05:32 +0100

    add human code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+/*
+-costruiamo un ciclo che conta da 1 a 100
+-dobbiamo verificare che il numero sia un multiplo di 3 o 5
+-SE abbiamo un numero divisibile per 3 e per 5 
+    -visualizza FizzBuzz
+ALTRIMENTI SE divisibile per 3
+    -display fizz al posto di 3 
+ALTRIMENTI SE divisibile per 5
+    -display buzz al posto di 5
+ALTRIMENTI
+    -display numero
+
+tools
+-dichiarazione variabili
+    -variabile per il contatore
+    -variabile per contenere fizz o buzz
+-ciclo for con contatore condizione e ++
+-condizione IF/ELSE per verificare la divisibilità
+-modulo che riporta il resto della divisibilità per le verifiche
+-operatore logico && per la divisibilità comune 3 e 5
+-console.log per far visualizzare il messaggio del numero contato o di fizz buzz
+*/
+
+
+
+
 
 
 
