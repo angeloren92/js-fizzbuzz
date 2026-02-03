d8c5bd60dc699eb8f0839d41de444933ad1413f4
angeloren92 <angeloren92@gmail.com>
2026-02-03 14:27:55 +0100

    add comments

--- js/script.js
+++ js/script.js
@@ -21,18 +21,30 @@ tools
 -console.log per far visualizzare il messaggio del numero contato o di fizz buzz
 */
 
+//inizializzazione, senza assegnazione della variabile
 let FizzOrBuzzOrN;
 
+//inizio ciclo for, da 1 a 100 compreso(<=)
 for (let i = 1; i <= 100; i++) {
+    //prima condizione che può essere anche solo %15 senza l'operatore logico come seconda soluzione possibile
     if (i % 3 === 0 && i % 5 === 0) {
+        //assegnazione valore alla variabile globale, in base alla prima verifica
         FizzOrBuzzOrN = 'FizzBuzz';
+        //altrimenti, seconda condizione verifichiamo la divisibilità per 3
     } else if (i % 3 === 0) {
+        //assegnazione valore alla variabile globale, in base alla seconda verifica 
         FizzOrBuzzOrN = 'Fizz';
+        //altrimenti, terza condizione verifichiamo la divisibilità per 5
     } else if (i % 5 === 0) {
+        //assegnazione valore alla variabile globale, in base alla terza verifica
         FizzOrBuzzOrN = 'Buzz';
+        //altrimenti, nessuna delle condizioni precedenti è veritiera
     } else {
+        /*assegniamo il valore del contatore alla variabile globale da visualizzare, 
+        in modo da non toccare il contatore, e ciò ci permette di fare un solo log finale */
         FizzOrBuzzOrN = i;
     }
+    //log finale della conta da 1 a 100
     console.log(FizzOrBuzzOrN)
 }
 
